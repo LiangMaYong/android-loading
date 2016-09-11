541c54fb9267599562f47a7e8d56a0a0239bd0b1
liangmayong <ibeam@qq.com>
2016-09-11 10:51:39 +0800

    fix bug

--- loading/src/main/java/com/liangmayong/loading/Loading.java
+++ loading/src/main/java/com/liangmayong/loading/Loading.java
@@ -76,21 +76,28 @@ public class Loading {
      */
     public static LoadingFragment showLoading(FragmentActivity activity, String label, int loadingColor, int backgroundColor, float dimAmount) {
         try {
-            DialogFragment loadingFragment = (DialogFragment) activity.getSupportFragmentManager()
-                    .findFragmentByTag(TAG);
-            loadingFragment = loadingFragment == null ? new LoadingFragment() : loadingFragment;
-            ((LoadingFragment) loadingFragment).setLabel(label);
-            ((LoadingFragment) loadingFragment).setLoadingColor(loadingColor);
-            ((LoadingFragment) loadingFragment).setBackgroundColor(backgroundColor);
-            ((LoadingFragment) loadingFragment).setDimAmount(dimAmount);
-            if (loadingFragment.isAdded()) {
-                activity.getSupportFragmentManager().beginTransaction().show(loadingFragment).commit();
-            } else {
-                loadingFragment.show(activity.getSupportFragmentManager(), TAG);
+            synchronized (activity) {
+                DialogFragment loadingFragment = (DialogFragment) activity.getSupportFragmentManager()
+                        .findFragmentByTag(TAG);
+                if (loadingFragment != null) {
+                    ((LoadingFragment) loadingFragment).setLabel(label);
+                    ((LoadingFragment) loadingFragment).setLoadingColor(loadingColor);
+                    ((LoadingFragment) loadingFragment).setBackgroundColor(backgroundColor);
+                    ((LoadingFragment) loadingFragment).setDimAmount(dimAmount);
+                    if (loadingFragment.isAdded()) {
+                        activity.getSupportFragmentManager().beginTransaction().show(loadingFragment).commit();
+                    }
+                } else {
+                    loadingFragment = new LoadingFragment();
+                    ((LoadingFragment) loadingFragment).setLabel(label);
+                    ((LoadingFragment) loadingFragment).setLoadingColor(loadingColor);
+                    ((LoadingFragment) loadingFragment).setBackgroundColor(backgroundColor);
+                    ((LoadingFragment) loadingFragment).setDimAmount(dimAmount);
+                    loadingFragment.show(activity.getSupportFragmentManager(), TAG);
+                }
+                return (LoadingFragment) loadingFragment;
             }
-            return (LoadingFragment) loadingFragment;
         } catch (Exception e) {
-            e.printStackTrace();
         }
         return null;
     }
